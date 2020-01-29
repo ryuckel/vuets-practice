0fdebd3595bbdc33ad1173c14a9449da7f75976c
ryusuke.miyaji <bluce826@gmail.com>
2020-01-29 20:21:39 +0900

    practice complete

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,10 +1,27 @@
 <template>
-  <button>MyButton</button>
+  <button @click="onClick">
+    <slot></slot>
+  </button>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Emit, Prop, Vue } from "vue-property-decorator";
 
 @Component
-export default class MyButton extends Vue {}
+export default class MyButton extends Vue {
+  private count: number = 0;
+
+  @Prop()
+  public greet?: string;
+
+  // 引数を追加しました
+  @Emit()
+  public click(count: number) {}
+
+  public onClick() {
+    alert(this.greet);
+    this.count++;
+    this.click(this.count);
+  }
+}
 </script>
